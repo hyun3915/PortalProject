3b0077d4f017afad590b37f5fb0bff178882de68
hyun3915 <soonsoon123@naver.com>
2021-01-11 12:25:49 +0900

    filedown

--- src/main/java/com/project/portal/board/notice/NoticeVO.java
+++ src/main/java/com/project/portal/board/notice/NoticeVO.java
@@ -1,6 +1,9 @@
 package com.project.portal.board.notice;
 
+import java.util.List;
+
 import com.project.portal.board.BoardVO;
+import com.project.portal.board.file.FileVO;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -12,5 +15,7 @@ public class NoticeVO extends BoardVO{
 	private String category;
 	private String department;
 	private String campus;
+	
+	private List<FileVO> files;
 
 }
--- src/main/java/com/project/portal/util/FileDown.java
+++ src/main/java/com/project/portal/util/FileDown.java
@@ -1,5 +1,60 @@
 package com.project.portal.util;
 
-public class FileDown {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.net.URLEncoder;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.servlet.view.AbstractView;
+
+import com.project.portal.board.file.FileVO;
+
+@Component("fileDown")
+public class FileDown extends AbstractView{
+	
+	@Autowired
+	private ResourceLoader resourceLoader;
+
+	@Override
+	protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+
+		String filePath = "classpath:/static/"+(String)model.get("filePath");
+		
+		FileVO fileVO = (FileVO)model.get("fileVO");
+		filePath = filePath+"/"+fileVO.getFileName();
+		System.out.println(filePath);
+		
+		File file = resourceLoader.getResource(filePath).getFile();
+		
+		response.setCharacterEncoding("UTF-8");
+		
+		response.setContentLengthLong(file.length());
+		
+		String fileName = URLEncoder.encode(fileVO.getOriName(), "UTF-8");
+		
+		response.setHeader("Content-Disposition", "attachment;filename=\""+fileName+"\"");
+		response.setHeader("Content-Transfer-Encoding", "binary");
+		
+		FileInputStream fi = new FileInputStream(file);
+		
+		OutputStream os = response.getOutputStream();
+		
+		FileCopyUtils.copy(fi, os);
+		
+		os.close();
+		fi.close();
+		
+	}
+	
+	
+	
 }
